5dc0c9cedae436ef1022f956561e6b691a3cbbb2
RaeZlo <rramirezperazzolo@gmail.com>
2025-04-21 11:18:24 -0300

    Create celsius_to_fahrenheit.py

---
+++ programacion-1/semana-1-fundamentos/ejercicios/celsius_to_fahrenheit.py
@@ -0,0 +1,11 @@
+"""
+Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+"""
+
+
+
+
+
+celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+fahrenheit = (celsius * 1.8) + 32
+print(f"La conversión de {celsius} °C a grados Fahrenheit es: {fahrenheit} °F")
